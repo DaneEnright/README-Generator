e5412ff6fef73dd4cf993b5a5f3610df658509d3
Dane Enright <enrightco@comcast.net>
2021-05-12 16:06:11 -0600

    added questions to index.js

---
+++ Develop/README.md
@@ -0,0 +1,9 @@
+Take an input from the command line and youneed to generate a Readme.md file based on the users answers.
+
+Tech Used: Inquirer, fs, Node, JS
+
+what questions do i need to ask? project name, project description, github user name, linkedin url, contributors, licenses
+
+build out questions with inquirer and then console log responses 
+
+once received responses build out functionality to create readme file - how do i format this in presentable way
---
+++ Develop/index.js
@@ -0,0 +1,66 @@
+// TODO: Include packages needed for this application
+
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github user name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of application:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What installations are needed for your application?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select any licenses that are applicable:",
+    choices: ["MIT", "ISC", "GPL", "None"],
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Were there any contributors on this application?",
+  },
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFile("README.md")
+// //   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+  //inquirer code user will see first question
+  inquirer.prompt(questions).then((data) => {
+    //figure out how to take those responses and write them to a file
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
+
+// Function call to initialize app
+init();
